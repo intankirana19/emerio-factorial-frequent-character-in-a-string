ff1a71986fb45f3fe3ee2bfb5da5a44dec9fa4fe
intankirana19 <intankirana19@gmail.com>
2021-01-16 01:35:47 +0700

    add function,reactive input form, button,output for most appears char

--- src/app/alphabet/alphabet.component.ts
+++ src/app/alphabet/alphabet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-alphabet',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alphabet.component.scss']
 })
 export class AlphabetComponent implements OnInit {
+  
+  str = new FormControl('');
+  maxChar: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  findCount() {
+    var max = 0;
+    this.maxChar = '';
+    this.str.value.split('').forEach( char => {
+      if(this.str.value.split(char).length > max) {
+          max = this.str.value.split(char).length;
+          this.maxChar = char;
+        }
+    });
   }
 
 }
