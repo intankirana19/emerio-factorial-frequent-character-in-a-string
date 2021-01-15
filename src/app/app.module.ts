b49f37ae61bbf3aad5fa75bf5817984b611e19b7
intankirana19 <intankirana19@gmail.com>
2021-01-16 00:30:57 +0700

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FactorialComponent } from './factorial/factorial.component';
+import { AlphabetComponent } from './alphabet/alphabet.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FactorialComponent,
+    AlphabetComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/factorial/factorial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-factorial',
+  templateUrl: './factorial.component.html',
+  styleUrls: ['./factorial.component.scss']
+})
+export class FactorialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
