42fafd0115c18d6b5c47117fd5fe55712bd1b686
intankirana19 <intankirana19@gmail.com>
2021-01-16 00:33:23 +0700

    add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlphabetComponent } from './alphabet/alphabet.component';
+import { FactorialComponent } from './factorial/factorial.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'factorial', component: FactorialComponent, pathMatch: 'full' },
+  { path: 'alphabet', component: AlphabetComponent },
+  { path: '',   redirectTo: '/factorial', pathMatch: 'full' }, 
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
