e69337a1ed782892f85133cf90b0f4d37a1cf5b4
intankirana19 <intankirana19@gmail.com>
2021-01-16 00:36:01 +0700

    add factorial function

--- src/app/factorial/factorial.component.ts
+++ src/app/factorial/factorial.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-factorial',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FactorialComponent implements OnInit {
 
+  number = new FormControl('');
+  factorial: number;
+  f: number;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  count() {
+    let f = 1;
+    this.factorial = this.number.value;
+    for(let i = 1; i <= this.factorial; i++) {
+      f = f * i;
+    }
+    this.factorial = f;
   }
 
 }
